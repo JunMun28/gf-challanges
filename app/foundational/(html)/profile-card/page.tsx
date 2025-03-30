9011413057af8fed75546c73042f2b97058f2fe9
JunMun28 <wongjunmun@gmail.com>
2025-03-30 13:03:37 +0800

    Update background gradient in ProfileCardPage for improved visual consistency.

--- app/foundational/(html)/profile-card/page.tsx
+++ app/foundational/(html)/profile-card/page.tsx
@@ -2,7 +2,7 @@ import ProfileCard from "@/components/ProfileCard";
 
 export default function ProfileCardPage() {
   return (
-    <div className="flex h-screen w-full justify-center items-start pt-[200px] bg-[linear-gradient(148deg,#FFFFFF_8.89%,#F0F4F8_100.48%)]">
+    <div className="flex h-screen w-full justify-center items-start pt-[200px] bg-[linear-gradient(148deg,#F9FAFB_8.89%,#D2D6DB_100.48%)]">
       <ProfileCard
         name="Sarah Dole"
         username="sarahdole"
